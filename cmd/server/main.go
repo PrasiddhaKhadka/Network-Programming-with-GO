aa7a71b4d3bd368c5ebc574682c4c978f06bf5d1
PrasiddhaKhadka <prasiddhak77@gmail.com>
2026-05-08 06:28:45 +0545

    Add

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+func main() {
+	list, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		panic(err)
+	}
+
+	defer list.Close()
+
+	for {
+		conn, err := list.Accept()
+
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		io.WriteString(conn, "Hello whats up!")
+		fmt.Println(conn, "How is your day")
+		fmt.Fprintf(conn, "%v", "Well! I hope")
+		conn.Close()
+
+	}
+
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,4 @@
 module prasiddhakhadka/np
 
 go 1.26.2
+
